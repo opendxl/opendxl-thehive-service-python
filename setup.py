abff96aafc7a4e60ef66c821803f2658c062a826
Chris Smith <csmith5@mcafee.com>
2018-05-15 09:21:28 -0700

    Update setup.py
    Updated classifiers

--- setup.py
+++ setup.py
@@ -113,13 +113,13 @@ setup(
     python_requires=">=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*",
 
     classifiers=[
-        "Programming Language : Python",
-        "Programming Language : Python :: 2",
-        "Programming Language : Python :: 2.7",
-        "Programming Language : Python :: 3",
-        "Programming Language : Python :: 3.4",
-        "Programming Language : Python :: 3.5",
-        "Programming Language : Python :: 3.6"
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ],
 
     cmdclass={
